a73049d2e04eed8658d729e5db32451fbdbf5329
OMID <ahmadpooromid@gmail.com>
2021-03-10 00:08:40 +0330

    remove unnecessary namespaces.

--- Sample/SampleForUseNeoBus/Startup.cs
+++ Sample/SampleForUseNeoBus/Startup.cs
@@ -1,7 +1,6 @@
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
