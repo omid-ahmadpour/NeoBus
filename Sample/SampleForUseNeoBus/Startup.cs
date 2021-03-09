4ce991df9f6b2fb00bca699a7721c7ed9c1fc69c
OMID <ahmadpooromid@gmail.com>
2021-03-10 00:06:24 +0330

    add swagger and register some services.

--- Sample/SampleForUseNeoBus/Startup.cs
+++ Sample/SampleForUseNeoBus/Startup.cs
@@ -4,6 +4,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using NeoBus.Kafka;
+using NeoBus.Kafka.EntityReplication;
+using NeoBus.MessageBus;
+using NeoBus.MessageBus.Abstractions;
 using NeoBus.MessageBus.Models;
 using SampleForUseNeoBus.ApplicationService;
 using SampleForUseNeoBus.ApplicationService.CommandHandlers;
@@ -17,8 +22,21 @@ namespace SampleForUseNeoBus
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication2", Version = "v1" });
+            });
+
             services.AddMediatR(typeof(Startup));
+
             services.AddScoped<CatalogUseCase>();
+
+            services.AddSingleton(typeof(KafkaProducer<>));
+
+            services.AddSingleton(typeof(KafkaEntityPublisher<,>));
+
+            services.AddScoped<IBus, Bus>();
 
             services.AddScoped<IRequestHandler<ProductAddCommand, CommandResult>, ProductAddCommandHandler>();
             services.AddScoped<INotificationHandler<ProductAddedEvent>, ProductAddedEventHandler>();
@@ -29,16 +47,19 @@ namespace SampleForUseNeoBus
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication2 v1"));
             }
+
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            //app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
