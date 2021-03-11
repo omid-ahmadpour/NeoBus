d8899cef2cb30b36db0aad9020d5bbe90831acff
OMID <ahmadpooromid@gmail.com>
2021-03-11 10:59:45 +0330

    modify ICanHandleQuery response.

--- NeoBus/MessageBus/Abstractions/ICanHandleQuery.cs
+++ NeoBus/MessageBus/Abstractions/ICanHandleQuery.cs
@@ -1,8 +1,9 @@
 ﻿using MediatR;
+using NeoBus.MessageBus.Models;
 
 namespace NeoBus.MessageBus.Abstractions
 {
-    public interface ICanHandleQuery<TCommand> : IRequestHandler<TCommand,TCommand> where TCommand : IRequest<TCommand>
+    public interface ICanHandleQuery<TRequest> : IRequestHandler<TRequest, CommandResult> where TRequest : IRequest<CommandResult>
     {
 
     }
