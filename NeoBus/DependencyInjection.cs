4871b716ac73331e821ccb95256323bbc1d1e542
OMID <ahmadpooromid@gmail.com>
2021-03-11 11:27:55 +0330

    add DependencyInjection to register services inside neobus.

---
+++ NeoBus/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using NeoBus.Kafka;
+using NeoBus.MessageBus;
+using NeoBus.MessageBus.Abstractions;
+using System.Reflection;
+
+namespace NeoBus
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddNeoBus(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddSingleton(typeof(KafkaProducer<>));
+
+            services.AddScoped<IBus, Bus>();
+
+            return services;
+        }
+    }
+}
